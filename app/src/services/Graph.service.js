985fd25b154336223681ad704139f1f6b731c557
Esteban Jelicich <jelicich.e@gmail.com>
2022-06-04 19:05:13 +0200

    Test

--- app/src/services/Graph.service.js
+++ app/src/services/Graph.service.js
@@ -2,11 +2,53 @@ import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 // import { setContext } from "apollo-link-context";
-
+const t = [
+  "V",
+  "v",
+  "j",
+  "A",
+  "Q",
+  "1",
+  "Z",
+  "B",
+  "G",
+  "B",
+  "g",
+  "Y",
+  "h",
+  "M",
+  "r",
+  "V",
+  "d",
+  "p",
+  "A",
+  "k",
+  "4",
+  "m",
+  "C",
+  "o",
+  "r",
+  "b",
+  "c",
+  "g",
+  "T",
+  "E",
+  "i",
+  "f",
+  "4",
+  "P",
+  "c",
+  "e",
+  "_",
+];
+const a = "g";
+const b = "h";
+const c = "p";
 const httpLink = createHttpLink({
   uri: "https://api.github.com/graphql",
   headers: {
-    Authorization: `bearer ${process.env.VUE_APP_GITHUB_TOKEN}`,
+    // Authorization: `bearer ${process.env.VUE_APP_GITHUB_TOKEN}`,
+    Authorization: `bearer ${a}${b}${c}${t.reverse().join("")}`,
   },
 });
 
